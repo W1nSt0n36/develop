e974c7703379df89427dd4c94b21f14c5e70de77
MIkhailGlukhov <115152932+W1nSt0n36@users.noreply.github.com>
2023-09-15 16:19:45 +0000

    dfwf

--- lab1/src/average.sh
+++ lab1/src/average.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-for ((a=1; a<150; a++))
+
+count=0
+number=0
+while read arg
 do
-	
+	count=$(($count+1))
+	number=$(($number+$arg))
 done
+dev=$(($number / $count))
+echo $dev
+echo $count
