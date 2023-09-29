8628f3c9fdc55d47b1f0f8b482f3b77707983d43
MIkhailGlukhov <115152932+W1nSt0n36@users.noreply.github.com>
2023-09-29 18:03:12 +0000

    fdgdg

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,15 @@
 
 void RevertString(char *str)
 {
-	// your code here
+    char m;
+	int len = 0;
+    while (str[len] != '\0') {
+        len++;
+    }
+    for (int i=len-1; i>=0; i--){
+        m +=str[i];
+
+    }
+
 }
 
